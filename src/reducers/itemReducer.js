66c0b29c2433a065ec03678fbc00392c18b41377
Gareth Slinn <gslinn@gmail.com>
2020-10-02 16:48:24 +0100

    add type constants

--- src/reducers/itemReducer.js
+++ src/reducers/itemReducer.js
@@ -1,8 +1,9 @@
 import { v4 as uuidv4 } from 'uuid';
+import { TYPES } from '../constants/types';
 
 const ItemReducer = (state, action) => {
     switch (action.type) {
-        case 'ADD_ITEM':
+        case TYPES.ADD_ITEM:
             const newName = {
                 id: uuidv4(),
                 name: action.payload
@@ -12,7 +13,7 @@ const ItemReducer = (state, action) => {
                 ...state,
                 people: [...state.people, newName]     
             }
-        case 'REMOVE_ITEM':
+        case TYPES.REMOVE_ITEM:
             const people = state.people;
             const remainingItems = people.filter(( item ) => {
                 return item.id !== action.payload;

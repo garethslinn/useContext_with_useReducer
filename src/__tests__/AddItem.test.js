9c127ace698eb05e90d590da8d591739944365f3
Gareth Slinn <gslinn@gmail.com>
2020-10-02 15:19:10 +0100

    apply add item tests

--- src/__tests__/AddItem.test.js
+++ src/__tests__/AddItem.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import App from '../App';
 
-
 const setup = () => {
     const utils = render(<App />)
     const input = utils.getByTestId('addButton');
@@ -21,5 +20,27 @@ test('can add new todo item', () => {
     const { input } = setup()
     fireEvent.change(input, { target: { value: 'text content' } })
     expect(input.value).toBe('text content')
+})
 
+test('add button is disabled on launch', () => {
+    const { getByTestId } = render(<App />)
+    const input = getByTestId('addButton');
+    expect(getByTestId('addButton')).toHaveAttribute('disabled');
+})
+
+test('button not disabled after adding text to input', () => {
+    const { getByTestId } = render(<App />);
+    const input = getByTestId('inputItem');
+    fireEvent.change(input, { target: { value: 'text content' } })
+    expect(getByTestId('addButton')).not.toHaveAttribute('disabled');
+})
+
+test('add button disabled when trying to add an empty input value', () => {
+    const { getByTestId } = render(<App />);
+    const input = getByTestId('inputItem');
+    fireEvent.change(input, { target: { value: 'text content' } })
+    fireEvent.click(getByTestId('addButton'));
+    fireEvent.change(input, { target: { value: '' } })
+    fireEvent.click(getByTestId('addButton'));
+    expect(getByTestId('addButton')).toHaveAttribute('disabled');
 })
